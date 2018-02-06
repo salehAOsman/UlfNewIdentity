8b16140dff48fe4139a209d6169cf1a2ba250bcc
Saleh Osman <saleh1.a.osman@gmail.com>
2018-02-06 09:31:29 +0100

    Add project files.

---
+++ UlfNewIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UlfNewIdentity.Startup))]
+namespace UlfNewIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
